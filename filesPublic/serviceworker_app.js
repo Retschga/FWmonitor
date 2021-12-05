8911b58b2f64f56c15cc5184270694ce79304eef
Retschga <johannes.sandbichl@gmail.com>
2021-12-05 17:35:32 +0100

    Fix: Alarmkarte

--- filesPublic/serviceworker_app.js
+++ filesPublic/serviceworker_app.js
@@ -2,7 +2,7 @@
 /* eslint-disable no-undef */
 'use strict';
 
-const staticCacheName = 'cache-vers-2021-12-05-001';
+const staticCacheName = 'cache-vers-2021-12-05-002';
 console.log('Loaded service worker! Cache Version ' + staticCacheName);
 
 const filesToCache = ['/app/offline'];
