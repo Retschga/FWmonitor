c39542871718fcc06183bcb3b03ebd8118b20bcb
Retschga <johannes.sandbichl@gmail.com>
2021-12-23 20:44:52 +0100

    Dixx: App Diashow

--- filesPublic/serviceworker_app.js
+++ filesPublic/serviceworker_app.js
@@ -2,7 +2,7 @@
 /* eslint-disable no-undef */
 'use strict';
 
-const staticCacheName = 'cache-vers-2021-12-17-001';
+const staticCacheName = 'cache-vers-2021-12-23-003';
 console.log('Loaded service worker! Cache Version ' + staticCacheName);
 
 const filesToCache = ['/app/offline'];
