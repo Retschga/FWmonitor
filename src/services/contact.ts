cb01c022f0c87ab629b64109caf45073a4497b8f
Retschga <johannes.sandbichl@gmail.com>
2022-02-10 16:02:21 +0100

    add: FWVV Kontakte aus Adressen.dbf

--- src/services/contact.ts
+++ src/services/contact.ts
@@ -1,10 +1,10 @@
 'use strict';
 
-import fs from 'fs';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import Dbfparser from '@episage/dbf-parser';
 import config from '../utils/config';
+import fs from 'fs';
 
 const NAMESPACE = 'Contact_Service';
 
@@ -18,26 +18,24 @@ type Contact = {
 };
 
 class Contactservice {
-    public async get_contacts_all(): Promise<Contact[] | undefined> {
+    private async get_contacts_fwvvMitglied(): Promise<Contact[] | undefined> {
         return new Promise((resolve) => {
             if (!config.fwvv.enabled) {
                 throw new Error(NAMESPACE + ' fwvv is not enabled');
             }
 
-            const parser = Dbfparser(fs.createReadStream(config.fwvv.dat_folder + '/MITGLIED.DBF'));
             const list: Contact[] = [];
 
+            const parser_mitglied = Dbfparser(
+                fs.createReadStream(config.fwvv.dat_folder + '/MITGLIED.DBF')
+            );
             /*  parser.on('header', (h: undefined) => {
                 //debug('dBase file header has been parsed');
                 //debug(h);
             }); */
 
             // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            parser.on('record', (record: any) => {
-                if (record['@deleted'] == true) {
-                    return;
-                }
-
+            parser_mitglied.on('record', (record: any) => {
                 if (record['@deleted'] == true) return;
                 if (!record.TEL && !record.TEL_DIENST && !record.MOBIL_TEL) return;
 
@@ -50,11 +48,50 @@ class Contactservice {
                     email: record.EMAIL
                 });
             });
-
-            parser.on('end', () => {
+            parser_mitglied.on('end', () => {
                 resolve(list);
             });
         });
+    }
+
+    private async get_contacts_fwvvAdressen(): Promise<Contact[] | undefined> {
+        return new Promise((resolve) => {
+            if (!config.fwvv.enabled) {
+                throw new Error(NAMESPACE + ' fwvv is not enabled');
+            }
+
+            const list: Contact[] = [];
+
+            const parser_adressen = Dbfparser(
+                fs.createReadStream(config.fwvv.dat_folder + '/ADRESSE.DBF')
+            );
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            parser_adressen.on('record', (record: any) => {
+                if (record['@deleted'] == true) return;
+                if (!record.TELEFON1 && !record.TELEFON2 && !record.MOBIL_TEL) return;
+
+                list.push({
+                    name: record.NAME,
+                    vorname: record.VORNAME,
+                    tel: record.TELEFON1,
+                    tel_dienst: record.TELEFON2,
+                    tel_mobil: record.MOBIL_TEL,
+                    email: record.EMAIL
+                });
+            });
+            parser_adressen.on('end', () => {
+                resolve(list);
+            });
+        });
+    }
+
+    public async get_contacts_all(): Promise<Contact[] | undefined> {
+        const list: Contact[] = [];
+        const list1 = await this.get_contacts_fwvvMitglied();
+        if (list1) list.push(...list1);
+        const list2 = await this.get_contacts_fwvvAdressen();
+        if (list2) list.push(...list2);
+        return list;
     }
 }
 
